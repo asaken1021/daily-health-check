b1bb2ffa733a894e2535f3299b9685f5d52c3fa1
asaken1021 <asaken1021@outlook.com>
2021-02-21 11:52:56 +0900

    [dev] add database

---
+++ backend/db/schema.rb
@@ -0,0 +1,30 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_21_021430) do
+
+  create_table "result", force: :cascade do |t|
+    t.integer "student_id"
+    t.float "temperature"
+    t.string "condition"
+    t.string "symptom"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "class"
+    t.integer "class_number"
+    t.string "name"
+  end
+
+end
